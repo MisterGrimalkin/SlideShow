c67e2115c25f3fba90f7a8d1fade9c1ef5aa010c
MisterGrimalkin <loki@amarantha.net>
2015-10-20 17:04:26 +0100

    Tidied up

--- src/main/java/slideshow/Main.java
+++ src/main/java/slideshow/Main.java
@@ -2,13 +2,11 @@ package slideshow;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -22,40 +20,70 @@ import java.util.*;
 
 public class Main extends Application {
 
+    /////////////////
+    // Application //
+    /////////////////
+
+    public static void main(String[] args) {
+        if ( args.length>0 ) {
+            startFolder = args[0];
+            if ( args.length>1 ) {
+                if ( "random".equalsIgnoreCase(args[1]) ) {
+                    // random not yet implemented
+                } else {
+                    try {
+                        setSize = Integer.parseInt(args[1]);
+                    } catch ( NumberFormatException ignored) {}
+                }
+                if ( args.length>2 ) {
+                    try {
+                        imageDuration = Integer.parseInt(args[2]) * 1000;
+                    } catch ( NumberFormatException ignored) {}
+                }
+            }
+            System.out.println("=====================================================");
+            System.out.println("  Slide Show\n    " + startFolder + "\n");
+            System.out.println("  Set Size: " + setSize);
+            System.out.println("  Delay: " + imageDuration + "ms");
+            System.out.println("=====================================================");
+            launch(args);
+        }
+    }
+
     private static String startFolder;
+    private static long imageDuration = 8000;
+    private static int setSize = 5;
 
-    private static boolean splitIntoBlocks = false;
 
-    private long showImageFor = 8000;
-    private int blockSize = 12;
-    private int imagesToShowPerBlock = 4;
-
-    private int width = 1700;
-    private int height = 2000;
-
-    private int currentBlock = 0;
-    private int imagesShownForCurrentBlock = 0;
-    private int totalFiles = 0;
-
-    private Map<Integer, List<ImageInfo>> imageBlocks = new HashMap<>();
-    private Set<String> filesDisplayed = new HashSet<>();
-
-    private GraphicsContext gc;
-    private Group board;
-
+    ////////
+    // UI //
+    ////////
 
     @Override
     public void start(Stage stage) throws Exception{
 
+        startSlideShow();
+
+        setupCanvas(stage);
+
+        new Timer(true).schedule(
+                new TimerTask() {
+                    @Override
+                    public void run() {
+                        nextImage();
+                    }
+                }, 0, imageDuration
+        );
+
+    }
+
+    private void setupCanvas(Stage stage) {
+
         width = (int)Math.floor(Screen.getPrimary().getVisualBounds().getWidth());
         height = (int)Math.floor(Screen.getPrimary().getVisualBounds().getHeight()) + 50;
 
-        loadImages(new File(startFolder));
-        System.out.println("Loaded " + totalFiles + " files, in " + (currentBlock+1) + " blocks.");
-        currentBlock = 0;
-
         final Pane pane = new Pane();
-        board = new Group();
+        Group board = new Group();
         pane.getChildren().add(board);
 
         stage.setScene(new Scene(pane, width, height));
@@ -64,184 +92,146 @@ public class Main extends Application {
         stage.show();
 
         Canvas canvas = new Canvas(width,height);
-        gc = canvas.getGraphicsContext2D();
-        gc.fillRect(10, 10, 10, 10);
-
         board.getChildren().add(canvas);
 
-        new Timer(true).schedule(
-                new TimerTask() {
-                    @Override
-                    public void run() {
-                        putRandomImages(2);
-                    }
-                }, 0, showImageFor
-        );
+        gc = canvas.getGraphicsContext2D();
 
         pane.setOnMouseClicked(event -> {
             if ( event.isControlDown() ) {
                 paused = !paused;
-                System.out.println(paused ? "Pause" : "Resume");
+                System.out.println(paused ? "|Pause|" : ">Resume>");
             }
         });
+
     }
+
+    private void drawImage(String filename) {
+
+        gc.fillRect(0, 0, width, height);
+
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(filename));
+        } catch (IOException ignored) {}
+
+        image = Scalr.resize(image, Scalr.Mode.FIT_TO_HEIGHT, width, height);
+        if ( image.getWidth() > width ) {
+            image = Scalr.resize(image, Scalr.Mode.FIT_TO_WIDTH, width);
+        }
+
+        Image img = SwingFXUtils.toFXImage(image, null);
+        gc.drawImage(img, (width-image.getWidth())/2, 0);
+
+    }
+
+    private GraphicsContext gc;
+
+    private int width = 1700;
+    private int height = 2000;
+
+
+    ////////////////
+    // Slide Show //
+    ////////////////
+
+    private void startSlideShow() {
+        buildImageList(new File(startFolder));
+        thinImageList();
+        setIterator = allImageFilenames.keySet().iterator();
+    }
+
+    private void nextImage() {
+        if ( !paused ) {
+            if ( imageIterator==null ) {
+                if ( !setIterator.hasNext() ) {
+                    startSlideShow();
+                }
+                imageIterator  = allImageFilenames.get(setIterator.next()).iterator();
+            }
+            if ( imageIterator.hasNext() ) {
+                drawImage(imageIterator.next());
+            } else {
+                imageIterator = null;
+                nextImage();
+            }
+        }
+    }
+
+
+    private void buildImageList(File dir) {
+
+        String dirName = dir.getName();
+        File[] files = dir.listFiles();
+
+        if ( files!=null ) {
+
+            Arrays.sort(files);
+
+            for (File file : files) {
+
+                if ( file.isDirectory() ) {
+                    buildImageList(file);
+                } else {
+                    if ( isImage(file) ) {
+                        List<String> filenames = allImageFilenames.get(dirName);
+                        if ( filenames==null ) {
+                            filenames = new LinkedList<>();
+                            allImageFilenames.put(dirName, filenames);
+                        }
+                        filenames.add(file.getPath());
+                    }
+                }
+            }
+
+        }
+    }
+
+    private void thinImageList() {
+        Random random = new Random(System.currentTimeMillis());
+        for (Map.Entry<String, List<String>> entry : allImageFilenames.entrySet() ) {
+            List<String> filenames = entry.getValue();
+            List<String> thinned = new LinkedList<>();
+            int i = 0;
+            int limit = Math.min(filenames.size(), setSize);
+            while ( i < limit ) {
+                int r = (int)Math.floor(random.nextDouble() * filenames.size());
+                String f = filenames.get(r);
+                if ( !thinned.contains(f) ) {
+                    thinned.add(f);
+                    i++;
+                }
+            }
+            Collections.sort(thinned);
+            allImageFilenames.put(entry.getKey(), thinned);
+        }
+    }
+
+    private boolean isImage(File file) {
+        if ( file.getName().length()>=4 ) {
+            String ext = file.getName().substring(file.getName().length() - 3);
+            return ext.equalsIgnoreCase("jpg")
+                    || ext.equalsIgnoreCase("peg")
+                    || ext.equalsIgnoreCase("png")
+                    || ext.equalsIgnoreCase("gif")
+                    || ext.equalsIgnoreCase("bmp");
+        }
+        return false;
+    }
+
+    private void printFileList() {
+        for (Map.Entry<String, List<String>> entry : allImageFilenames.entrySet() ) {
+            System.out.println(entry.getKey());
+            for ( String s : entry.getValue() ) {
+                System.out.println("\t" + s);
+            }
+        }
+    }
+
+    private Map<String, List<String>> allImageFilenames = new LinkedHashMap<>();
+
+    private Iterator<String> setIterator = null;
+    private Iterator<String> imageIterator = null;
 
     private boolean paused = false;
-
-
-
-    private void putRandomImages(int count) {
-        if ( !paused ) {
-
-            if ( count > totalFiles ) {
-                count = totalFiles;
-            }
-
-            int left = 0;
-            gc.fillRect(0, 0, width, height);
-            int i = 0;
-            int retries = 0;
-            List<BufferedImage> nowImages = new ArrayList<>();
-            while (i < count) {
-
-                if ( imagesShownForCurrentBlock > imagesToShowPerBlock ) {
-                    currentBlock++;
-                    imagesShownForCurrentBlock = 0;
-                }
-                String filename = getRandomImage();
-
-                if (filename != null && !filesDisplayed.contains(filename)) {
-                    BufferedImage image = null;
-                    try {
-                        image = ImageIO.read(new File(filename));
-                        filesDisplayed.add(filename);
-                    } catch (IOException e) {
-                    }
-                    if (image != null) {
-                        retries = 0;
-                        image = Scalr.resize(image, Scalr.Mode.FIT_TO_HEIGHT, 1000 / count, 1000);
-                        if ( image.getWidth() > width ) {
-                            image = Scalr.resize(image, Scalr.Mode.FIT_TO_WIDTH, width);
-                        }
-                        if (image != null) {
-                            if (left + image.getWidth() <= width) {
-                                nowImages.add(image);
-                                filesDisplayed.add(filename);
-                                System.out.println(filename);
-                                imagesShownForCurrentBlock++;
-                                left += image.getWidth();
-                            }
-                            i++;
-                        }
-                    }
-                } else {
-                    if (++retries > 200) {
-                        filesDisplayed.clear();
-                        currentBlock++;
-                        imagesShownForCurrentBlock = 0;
-                        retries = 0;
-                    }
-                }
-            }
-
-            int x0 = (width - left) / (nowImages.size() + 1);
-            int x = x0;
-            for (BufferedImage nowImage : nowImages) {
-                Image img = SwingFXUtils.toFXImage(nowImage, null);
-                gc.drawImage(img, x, 0);
-                x += nowImage.getWidth() + x0;
-            }
-        }
-
-    }
-
-
-    private String getRandomImage() {
-
-        if ( !splitIntoBlocks || currentBlock<imageBlocks.size() ) {
-            List<ImageInfo> infos = imageBlocks.get(currentBlock);
-            if ( infos!=null && !infos.isEmpty() ) {
-                int index = (int)Math.floor(Math.random()*(infos.size()));
-                ImageInfo info = infos.get(index);
-                return info.filename;
-            } else {
-                currentBlock++;
-                imagesShownForCurrentBlock = 0;
-            }
-
-        } else {
-            currentBlock = 0;
-            imagesShownForCurrentBlock = 0;
-        }
-        return null;
-    }
-
-    private void loadImages(File dir) {
-        try {
-            File[] files = dir.listFiles();
-            if ( files!=null ) {
-                Arrays.sort(files);
-                int fileCount = 0;
-                for (File file : files) {
-                    if (file.isDirectory() ) {
-                        if ( splitIntoBlocks ) { currentBlock++; }
-                        loadImages(file);
-                    } else {
-                        if ( file.getName().length()>=4 ) {
-                            String ext = file.getName().substring(file.getName().length() - 3);
-                            if (      ext.equalsIgnoreCase("jpg")
-                                    | ext.equalsIgnoreCase("peg")
-                                    | ext.equalsIgnoreCase("png")
-                                    | ext.equalsIgnoreCase("gif")
-                                    | ext.equalsIgnoreCase("bmp")
-                            ) {
-                                if (!splitIntoBlocks || fileCount < blockSize) {
-                                    List<ImageInfo> infos = imageBlocks.get(currentBlock);
-                                    if (infos == null) {
-                                        infos = new ArrayList<>();
-                                        imageBlocks.put(currentBlock, infos);
-                                    }
-                                    infos.add(new ImageInfo(file.getCanonicalPath()));
-//                                    System.out.println(file.getName());
-                                    totalFiles++;
-                                    fileCount++;
-                                } else {
-                                    fileCount = 0;
-                                    currentBlock++;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        if ( args.length>0 ) {
-            startFolder = args[0];
-            if ( args.length>1 ) {
-                splitIntoBlocks = "seq".equalsIgnoreCase(args[1]);
-            }
-            System.out.println("Starting SlideShow: " + startFolder);
-            launch(args);
-        }
-    }
-
-    private static class ImageInfo {
-        String filename;
-        int width = 0;
-        int height = 0;
-        public ImageInfo(String filename) {
-            this.filename = filename;
-        }
-        public ImageInfo(String filename, int width, int height) {
-            this.filename = filename;
-            this.width = width;
-            this.height = height;
-        }
-    }
 
 }
